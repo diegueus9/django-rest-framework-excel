971ee1a28febb24832ba01678ee02f6cc86df32b
Diego Andrés Sanabria Martín <diegueus9@gmail.com>
2015-10-13 10:42:38 -0500

    Initial version

---
+++ rest_framework_excel/renderers.py
@@ -0,0 +1,140 @@
+from __future__ import unicode_literals
+from six import StringIO, text_type
+
+from ordereddict import OrderedDict
+from openpyxl import Workbook
+
+from rest_framework.renderers import BaseRenderer
+
+# six versions 1.3.0 and previous don't have PY2
+try:
+    from six import PY2
+except ImportError:
+    import sys
+    PY2 = sys.version_info[0] == 2
+
+
+class ExcelRenderer(BaseRenderer):
+
+    """
+    Renderer which serializes to Excel
+    """
+
+    media_type = 'application/ms-excel'
+    format = 'xlsx'
+    level_sep = '.'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        """
+        Renders serialized *data* into Excel. For a dictionary:
+        """
+        if data is None:
+            return ''
+
+        stream = StringIO()
+
+        wb = Workbook(encoding="UTF-8")
+        ws = wb.active
+
+        table = self.tablize(data)
+
+        for row_idx, row in enumerate(table, 1):
+            for col_idx, elem in enumerate(row, 1):
+                if isinstance(elem, text_type) and PY2:
+                    ws.cell(column=col_idx, row=row_idx, value=elem.encode('utf-8'))
+                else:
+                    ws.cell(column=col_idx, row=row_idx, value=elem)
+
+        wb.save(stream)
+
+        return stream.getvalue()
+
+    def tablize(self, data):
+        """
+        Convert a list of data into a table.
+        """
+        if data:
+
+            # First, flatten the data (i.e., convert it to a list of
+            # dictionaries that are each exactly one level deep).  The key for
+            # each item designates the name of the column that the item will
+            # fall into.
+            data = self.flatten_data(data)
+
+            # Get the set of all unique headers, and sort them.
+            headers = []
+            if data[0]:
+                headers = data[0].keys()
+
+            # Create a row for each dictionary, filling in columns for which the
+            # item has no data with None values.
+            rows = []
+            for item in data:
+                row = []
+                for key in headers:
+                    row.append(item.get(key, None))
+                rows.append(row)
+
+            # Return your "table", with the headers as the first row.
+            return [headers] + rows
+
+        else:
+
+            return []
+
+    def flatten_data(self, data):
+        """
+        Convert the given data collection to a list of dictionaries that are
+        each exactly one level deep. The key for each value in the dictionaries
+        designates the name of the column that the value will fall into.
+        """
+        flat_data = []
+        for item in data:
+            flat_item = self.flatten_item(item)
+            flat_data.append(flat_item)
+
+        return flat_data
+
+    def flatten_item(self, item):
+        if isinstance(item, list):
+            flat_item = self.flatten_list(item)
+        elif isinstance(item, dict):
+            flat_item = self.flatten_dict(item)
+        else:
+            flat_item = {'': item}
+
+        return flat_item
+
+    def nest_flat_item(self, flat_item, prefix):
+        """
+        Given a "flat item" (a dictionary exactly one level deep), nest all of
+        the column headers in a namespace designated by prefix.  For example:
+         header... | with prefix... | becomes...
+        -----------|----------------|----------------
+         'lat'     | 'location'     | 'location.lat'
+         ''        | '0'            | '0'
+         'votes.1' | 'user'         | 'user.votes.1'
+        """
+        nested_item = {}
+        for header, val in flat_item.items():
+            nested_header = self.level_sep.join([prefix, header]) if header else prefix
+            nested_item[nested_header] = val
+        return nested_item
+
+    def flatten_list(self, l):
+        flat_list = {}
+        for index, item in enumerate(l):
+            index = text_type(index)
+            flat_item = self.flatten_item(item)
+            nested_item = self.nest_flat_item(flat_item, index)
+            flat_list.update(nested_item)
+        return flat_list
+
+    def flatten_dict(self, d):
+        flat_dict = OrderedDict()
+        for key, item in d.iteritems():
+            key = str(key)
+            flat_item = self.flatten_item(item)
+            nested_item = self.nest_flat_item(flat_item, key)
+            flat_dict.update(nested_item)
+        return flat_dict
---
+++ setup.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import re
+import os
+import sys
+
+
+name = 'djangorestframework-excel'
+package = 'rest_framework_excel'
+description = 'Excel Render for Django REST Framework'
+url = 'https://github.com/diegueus9/django-rest-framework-excel'
+author = 'Diego Andres Sanabria'
+author_email = 'diegueus9@gmail.com'
+license = 'Apache'
+install_requires = ['djangorestframework', 'openpyxl', 'ordereddict', 'six']
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    args = {'version': get_version(package)}
+    print "You probably want to also tag the version now:"
+    print "  git tag -a %(version)s -m 'version %(version)s'" % args
+    print "  git push --tags"
+    sys.exit()
+
+
+setup(
+    name=name,
+    version=get_version(package),
+    url=url,
+    license=license,
+    description=description,
+    author=author,
+    author_email=author_email,
+    packages=get_packages(package),
+    package_data=get_package_data(package),
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Framework :: Django",
+    ],
+)
